ada4b965790ff9d83fe3098d3f8fd2e474eb4054
Johnny Ji <johnny@johnnyji.com>
2015-08-29 01:01:21 -0700

    change to terinary operator

--- src/server/routes/authRoute.js
+++ src/server/routes/authRoute.js
@@ -6,17 +6,13 @@ const router = express.Router();
 
 router.post('/register', (req, res, next) => {
   User.register(req.body.user)
-    .then((user) => {
-      res.status(201).json({ user: user });
-    })
-    .catch((err) => {
-      res.status(422).json({ message: err });
-    });
+    .then(user => res.status(201).json({ user: user }))
+    .catch(err => res.status(422).json({ message: err }));
 });
 
 router.post('/login', (req, res, next) => { // TODO: Hash and salt password
   User.findAndAuthenticate(req.body.user)
-    .then((user) => {
+    .then(user => {
       // create a jsonwebtoken on user authenticate
       let token = jwt.sign(user, app.get('tokenSecret'), {
         expiresInMinutes: 1440 // 24 hrs
@@ -26,9 +22,7 @@ router.post('/login', (req, res, next) => { // TODO: Hash and salt password
         token: token
       });
     })
-    .catch((err) => {
-      res.status(401).json({ message: err });
-    });
+    .catch(err => res.status(401).json({ message: err }));
 });
 
 export default router;
