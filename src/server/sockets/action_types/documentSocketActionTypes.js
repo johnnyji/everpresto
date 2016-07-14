a09573919129993aa7ec462d30b02ca8a3bd539f
Johnny Ji <johnny@johnnyji.com>
2016-07-14 00:59:09 -0700

    temporary patch for import/export error, needs further investigation

--- src/server/sockets/action_types/documentSocketActionTypes.js
+++ src/server/sockets/action_types/documentSocketActionTypes.js
@@ -1,6 +1,7 @@
-import createConstants from '../../../client/action_types/utils/createConstants';
+// TODO: Why can we not use import/export syntax here? Throws `invalid token import` error
+const createConstants = require('../../../client/action_types/utils/createConstants');
 
-export default createConstants([
+module.exports = createConstants([
   'DOCUMENT_SEND_EMAIL_ERROR',
   'DOCUMENT_SEND_EMAIL_SUCCESS',
   'DOCUMENT_SEND_EMAILS_COMPLETE'
--- src/server/sockets/utils/config.js
+++ src/server/sockets/utils/config.js
@@ -1,6 +1,7 @@
-import config from '../../../../config/config';
+// TODO: Why can we not use import/export syntax here? Throws `invalid token import` error
+const config = require('../../../../config/config');
 
-export default {
+module.exports = {
   paths: {
     client: {
       collections: `${config.baseUrl}/collections`,
